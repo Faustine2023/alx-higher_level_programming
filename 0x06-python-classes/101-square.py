c5c1421ec975327adffb6e2bb67c89a1f1ccda92
Faustine2023 <faustinemudambi835@gmail.com>
2023-08-30 11:11:14 +0300

    A2

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -53,7 +53,7 @@ class Square:
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for l in range(0, self.__position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
 
@@ -62,7 +62,7 @@ class Square:
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for l in range(0, self.__position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
