8c79b4ec584328780aac55479e88ffa08a744c6e
Faustine2023 <faustinemudambi835@gmail.com>
2023-12-14 13:21:10 -0800

    comiits

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Lists genres from database hbtn_0d_tvshows along with number 
+-- shows linked towards each
+-- Doesn't display genres without linked shows
+-- Records are ordered by descending number of shows linked
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
